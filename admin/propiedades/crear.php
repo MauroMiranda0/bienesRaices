a5a41cdf052ac5a2d3a7bc0e0dd79fffe4742a40
MauroMiranda0 <maurovergara04@gmail.com>
2025-03-05 21:03:17 -0600

    commitCamposValorPrevio

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,14 @@
     //Arreglo con mensajes de errores
     $errores = [];
 
+    $titulo = '';
+    $precio = '';
+    $descripcion = '';
+    $habitaciones = '';
+    $wc = '';
+    $estacionamiento = '';
+    $vendedores_id = '';
+
     //Ejecutar el codigo despues de que el usuario envia el formulario
     if($_SERVER['REQUEST_METHOD']==='POST'){
         /* echo "<pre>";
@@ -19,7 +27,7 @@
         $habitaciones = $_POST['habitaciones'];
         $wc = $_POST['wc'];
         $estacionamiento = $_POST['estacionamiento'];
-        $vendedor_id = $_POST['vendedor'];
+        $vendedores_id = $_POST['vendedor'];
 
         if(!$titulo){
             $errores[] = "Debes añadir un titulo";
@@ -40,7 +48,7 @@
             $errores[] = "Debes añadir un número de lugares de estacionamiento";
         }
 
-        if(!$vendedor_id){
+        if(!$vendedores_id){
             $errores[] = "Debes elegir un vendedor";
         }
 
@@ -51,7 +59,7 @@
         //Revisar que el arreglo de errores este vacio
         if(empty($errores)){
             //Insertar en la Base de Datos
-            $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedor_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedor_id') ";
+            $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedores_id') ";
 
             //echo query
             $resultado = mysqli_query($db, $query);
@@ -88,25 +96,25 @@
             <fieldset>
                 <legend>Información General</legend>
                 <label for="titulo">Titulo</label>
-                <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value="<?php echo $titulo; ?>">
 
                 <label for="precio">Precio</label>
-                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $precio; ?>">
 
                 <label for="imagen">Imagen</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripción</label>
-                <textarea id="descripcion" name="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
             </fieldset>
             <fieldset>
                 <legend>Información Propiedad</legend>
                 <label for="habitaciones">Habitaciones</label>
-                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej. 3" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej. 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
                 <label for="wc">Baños</label>
-                <input type="number" id="wc" name="wc" placeholder="Ej. 3" min="1" max="9">
+                <input type="number" id="wc" name="wc" placeholder="Ej. 3" min="1" max="9" value="<?php echo $wc; ?>">
                 <label for="estacionamiento">Estacionamiento</label>
-                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej. 3" min="1" max="9">
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej. 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
             </fieldset>
             <fieldset>
                 <legend>Vendedor</legend>
--- includes/config/database.php
+++ includes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost:3306', 'root', '', 'bienesraices_crud');
+    $db = mysqli_connect('localhost:8080', 'root', 'root', 'bienesraices_crud');
     if (!$db) {
         echo "Error NO se pudo conectar";
         exit;
